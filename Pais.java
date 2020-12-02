962f3e4046d6688675286b00bd413266b459501f
AngelBandin <73777868+AngelBandin@users.noreply.github.com>
2020-12-02 14:14:49 +0100

    Update Pais.java

--- Pais.java
+++ Pais.java
@@ -3,28 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Continentes;
-import java.awt.Color;
-import java.util.ArrayList;
-import Ejercitos.Ejercito;
-import Jugadores.Jugador;
+package risk;
+
 import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author angel
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  *
  * @author Santiago Golpe
  */
 public class Pais {
     private String Nombre;
-    private Color color;
-    private Jugador nombre_j;
+    private String color;
+    public Jugador nombre_j;
     private Ejercito Ejercito;
     private Continente Continente;
     private ArrayList<Point> Fronteras;
+    private int vecesocupado;
     
-    public Pais(){
-        
-    }
-    public Pais(String nombre, Continente continente,ArrayList<Point> L ){
+   public Pais(String nombre, Continente continente,ArrayList<Point> L ){
       this.Nombre=nombre;
       this.Continente=continente;
       this.color=continente.getColorcontinente();
@@ -36,13 +43,24 @@ public class Pais {
     public Jugador getJugador(){
         return this.nombre_j;
     }
-    public Color getColor(){
+    public String getColor(){
         return this.color;
     }
-    public ArrayList getFrontera(){
+    //Creacion de paises
+   
+
+
+    /*rivate int numeroEjercitos;
+        private boolean tieneJugador;
+    private String abrev;
+    */
+    public ArrayList<Point> getFronteras(){
         return this.Fronteras;
     }
-    
-    //Creacion de paises
-  
+    public void describir_pais(){
+        System.out.println("nombre: "+nombre_j.getNombreJugador()+"abreviatura: "+this.Nombre+", continente: "+this.Continente.getContinente()+", frontera: ");
+        this.Fronteras.forEach((k) -> System.out.println(k+" "));
+        System.out.println("], jugador: "+ this.nombre_j.getNombreJugador()+", numeroEjercitos"+ this.Ejercito.getEjnum()+", numeroVecesOcupado"+ this.vecesocupado);
+    }   		
 }
+
