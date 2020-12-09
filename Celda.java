207a073e8aa5ead723ffc7707a3cea9a93c755b3
Eplogaitnas <73778295+Eplogaitnas@users.noreply.github.com>
2020-12-09 12:10:43 +0100

    Update Celda.java

--- Celda.java
+++ Celda.java
@@ -7,6 +7,7 @@ package Mapa;
 import Continentes.Pais;
 import Jugadores.Jugador;
 import java.awt.Color;
+import risk.Ejercito;
 /**
  *
  * @author Santiago Golpe
@@ -17,17 +18,14 @@ public class Celda {
     private Jugador jugador;
     private String Celda;
     private int Clase;
+    private Ejercito ejercito;
     
     public Celda(){
         this.tipo="Oceano";
-        this.pais=null;
-        this.jugador=null;
     }
     
     public Celda(String x, int classe){
         this.tipo="Frontera Oceanica";
-        this.pais=null;
-        this.jugador=null;
         this.Celda=x;
         this.Clase=classe;
     }
@@ -38,86 +36,104 @@ public class Celda {
         this.jugador=p.getJugador();
     }
     
+    public Ejercito getejercito(){
+        return this.ejercito;
+    }
     
     public void settipoCelda(String Tipo){
         this.tipo=Tipo;
     }
-     public void setpaisCelda(Pais P){
+    
+    public void setpaisCelda(Pais P){
         this.pais=P;
     }
-      public void setjugadorCelda(Jugador j){
+    
+    public void setjugadorCelda(Jugador j){
         this.jugador=j;
     }
       
-      public String getTipoCelda(){
+    public String getTipoCelda(){
           return this.tipo;
-      }
-      public Pais getPaisCelda(){
+    }
+    
+    public Pais getPaisCelda(){
           return this.pais;
-      }
-      public Jugador getjugadorCelda(){
+    }
+    
+    public Jugador getjugadorCelda(){
           return this.jugador;
-      }
-      public String getCeldaCelda(){
+    }
+    
+    public String getCeldaCelda(){
           return this.Celda;
       }
-      public int getclase(){
+      
+    public int getclase(){
           return this.Clase;
-      }
+    }
         //Funciones para imprimir celdas
-        public String devuelveCelda(){
-            String aux=null;
-            if(this.tipo.equals("Pais")){
-               aux=pais.getNombrePais();
-            }
-            if(this.tipo.equals("Oceano")){
-               aux="|                                   |";
-            }
-            if(this.tipo.equals("Frontera Oceanica")){
-                aux=this.Celda;
-            }
-            return aux;
+       
+    public String devuelveCelda(){
+        String aux=null;
+        if(this.tipo.equals("Pais")){
+           aux=pais.getNombrePais();
         }
+        if(this.tipo.equals("Oceano")){
+           aux="|                                   |";
+        }
+        if(this.tipo.equals("Frontera Oceanica")){
+            aux=this.Celda;
+        }
+        return aux;
+    }
         //Imrimir celdas
-        public void imprimeCelda(){
-            String aux=String.format("|"+"                 "+"%-9s"+"         |", pais.getNombrePais());
-            if(pais.getColor().equals(Color.red)){
-                aux=String.format("|"+"\u001B[41m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
-            if(pais.getColor().equals(Color.cyan)){
-                aux=String.format("|"+"\u001B[46m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
-            if(pais.getColor().equals(Color.green)){
-                aux=String.format("|"+"\u001B[42m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
-            if(pais.getColor().equals(Color.magenta)){
-                aux=String.format("|"+"\u001B[45m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
-            if(pais.getColor().equals(Color.blue)){
-                aux=String.format("|"+"\u001B[44m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
-            if(pais.getColor().equals(Color.yellow)){
-                aux=String.format("|"+"\u001B[43m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
-            }
+    public void imprimeCelda(){
+        String aux=String.format("|"+"                 "+"%-9s"+"         |", pais.getNombrePais());
+        if(pais.getColor().equals("red")){
+            aux=String.format("|"+"\u001B[41m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        if(pais.getColor().equals("cyan")){
+            aux=String.format("|"+"\u001B[46m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        if(pais.getColor().equals("green")){
+            aux=String.format("|"+"\u001B[42m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        if(pais.getColor().equals("magenta")){
+            aux=String.format("|"+"\u001B[45m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        if(pais.getColor().equals("blue")){
+            aux=String.format("|"+"\u001B[44m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        if(pais.getColor().equals("yellow")){
+            aux=String.format("|"+"\u001B[43m"+"                 "+"%-9s"+"         "+"\u001B[0m"+"|", pais.getNombrePais());
+        }
+        System.out.print(aux);
+    }
+        
+    public void imprimeCeldafrontO(){
+        String aux=String.format("\u001B[31m"+"%s"+"\u001B[0m", Celda);        
+        if(Clase==1){
+            System.out.print("\u001B[31m"+"|-----------------------------------|"+ "\u001B[0m");
+        }
+        if(Clase==2){
+            System.out.print("\u001B[31m"+"|                    |               |"+ "\u001B[0m");
+        }
+    }
+        
+    public void imprimeCeldaOc(){
+        System.out.print("|                                   |");
+         String aux=String.format("|                                   |");
+    }
+        
+    public void imprimeejercito(){
+        if(this.tipo.equals("Pais")){
+            String aux=String.format("%d", this.pais.getEjercito().getnumero());
             System.out.print(aux);
-            
         }
-        public void imprimeCeldafrontO(){
-            String aux=String.format("\u001B[31m"+"%s"+"\u001B[0m", Celda);
-            
-            
-            if(Clase==1){
-                System.out.print("\u001B[31m"+"|-----------------------------------|"+ "\u001B[0m");
-            }
-            if(Clase==2){
-                System.out.print("\u001B[31m"+"|                    |               |"+ "\u001B[0m");
-            }
+        else{
+            System.out.print(" ");
         }
-        public void imprimeCeldaOc(){
-            System.out.print("|                                   |");
-             String aux=String.format("|                                   |");
         }
-        //Creacion de celdas
         
 }
 
